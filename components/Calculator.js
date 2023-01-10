e6a0c3ccfd19f68c8632a2b65767b6eb1e845b76
inakiesk <inakieskruiz@gmail.com>
2023-01-10 13:47:25 -0600

    Add Calculator component with basic display

---
+++ components/Calculator.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: 0,
+    };
+  }
+  render() {
+    const { display } = this.state;
+    return (
+      <div className="calculator">
+        { display }
+      </div>
+    )
+  }
+}
