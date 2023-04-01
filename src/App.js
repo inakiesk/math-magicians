08d656cee1509007f7c42b5df56618c6d49ddbcd
inakiesk <inakieskruiz@gmail.com>
2023-04-01 17:49:46 -0600

    Add routes to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
 import ApiQuote from './components/ApiQuote';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <ApiQuote />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Calculator" element={<Calculator />} />
+        <Route path="Quote" element={<ApiQuote />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
